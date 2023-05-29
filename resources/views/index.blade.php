c640a5fd95597df205ca40afe0d8d267e1df85b8
finaOrivia <finaon83@gmail.com>
2023-05-29 16:00:01 +0700

    delete . cuy

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,6 @@
 @section('title', 'Dashboard')
 @section('content')
 <div class="quotes">
-    <p>“Tresno kuwi koyok gapuro, seng awal e selamat datang jebul e tetep selamat tinggal.”</p>
+    <p>“Tresno kuwi koyok gapuro, seng awal e selamat datang jebul e tetep selamat tinggal”</p>
 </div>
 @endsection
