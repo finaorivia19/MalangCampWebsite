1305f2bcfdbf5d1c4529c401621a8e53e7a7536b
AkbarIhsan <93473539+AkbarIhsan@users.noreply.github.com>
2023-06-08 12:35:10 +0700

    update login dan lupa pw

--- resources/views/layoutLogin.blade.php
+++ resources/views/layoutLogin.blade.php
@@ -6,6 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title') | Malang Camp</title>
 
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
     <style>
         *{
             margin: 0;
@@ -37,6 +39,10 @@
             float: left;
             width: 57%;
             height: 93vh;
+        }
+
+        .box-form-login form{
+            margin-top:10%;
         }
 
         .kolom{
--- resources/views/lupaPassword.blade.php
+++ resources/views/lupaPassword.blade.php
@@ -6,6 +6,7 @@
         .kolom{
             text-align: left;
             padding-top: 3vh;
+            width: 100%;
         }
 
         .kolom input{
@@ -13,7 +14,7 @@
             margin: 0 auto;
             background: #fff;
             border-radius: 10px;
-            width: 77%;
+            width: 100%;
             height: 41px;
             font-family: 'Inter';
             font-style: normal;
@@ -25,16 +26,16 @@
 </style>
 
 <div class="all" style="padding-top:40px;">
-<div class="d-flex" id="table" style=" background-color: #96858F; padding: 25px; border-radius: 35px; margin-left:85px; width: 800px;">
+<div class="" id="table" style=" background-color: #96858F; padding: 25px; border-radius: 35px; margin-left:85px; width: 800px;">
 
-<div class="card mb-3" style="width: 100%; border-radius:20px; background-color: #D9D9D9;">
-        <div class="col-md-8">
+<div class="" style="width: 100%; border-radius:20px; background-color: #D9D9D9;">
+        <div class="">
             <div class="card-body">
             <form method="post" action="" id="myForm" enctype="multipart/form-data">
                     @csrf
 
                     <div class="kolom">
-                                <input id="email" placeholder="Email Address" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus style="background:white; margin-bottom:30px; outline:0;">
+                                <input id="email" placeholder="Email Address" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus style="background:white; margin-bottom:30px; outline:0; width:50%;">
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -44,7 +45,7 @@
 
                         <div class="kolom">
                             <div class="col-md-6">
-                                <input id="password" placeholder="Password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" style="background:white;outline:0;margin-bottom:30px;">
+                                <input id="password" placeholder="Password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" style="background:white;outline:0;margin-bottom:30px; width:100%;">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -60,10 +61,12 @@
                 <h6 class="mb-4 card-title" style="height:23px; width: 400px; background-color:white; border-radius:10px; padding-left:8px;"></h6>
                 <h5 class="mb-3 card-title" style="height:23px; width: 400px; background-color:white; border-radius:10px; padding-left:8px;"></h5>
                 <h6 class="mb-4 card-title" style="height:23px; width: 400px; background-color:white; border-radius:10px; padding-left:8px;"></h6> -->
-                
-                    <div class="button-cncl-updt" style="margin-left:20vh">
+
+                  <center>
+                    <div class="button-cncl-updt">
                         <a href="account" type="button" class="btn btn-outline-light"style="background-color:#673A54;margin-left:10vh">Kirim</a>
                     </div>
+                  </center>
 
             </div>
         </div>
